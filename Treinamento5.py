2902c4da834e4642b32c3350ed0e5a501d7cb06f
Frederic Menezes Ferreira <fredericmferreira@gmail.com>
2024-05-27 09:47:41 -0300

    Create Treinamento5.py

---
+++ Treinamento5.py
@@ -0,0 +1,89 @@
+import os
+import wandb
+import time
+import random
+import numpy as np
+#import gym
+
+#from stable_baselines3 import PPO
+from stable_baselines3.common.monitor import Monitor
+#from stable_baselines3.common.vec_env import DummyVecEnv, VecVideoRecorder
+from Stablebaselines3.dummy_vec_env import DummyVecEnv
+
+from Ambiente_SOMN.make_env import make_env
+from Stablebaselines3.PPO import PPO
+from Ambiente_SOMN.Yard import Yard
+
+# Initialize a new wandb run
+if len(wandb.patched["tensorboard"]) > 0:
+    wandb.tensorboard.unpatch()
+#wandb.tensorboard.patch(root_logdir="/content/drive/MyDrive/SOMN2/runs")
+wandb.tensorboard.patch(root_logdir="./runs")
+
+# seed
+#random.seed(10)
+#np.random.seed(1)
+from Seed.Seed import seed_everything
+
+#seed_everything(2023)
+
+#atraso:int=None
+objetivo = ["Lucro", "Variabilidade", "Sustentabiliade"]
+for atraso in range(-1,0,10):  ### ACMO USAR UMA COMBINAÇÃO QUE DESABILITE
+#    atraso = None
+    config_PPO = {
+        'objetivo': 0, # 0: lucro, 1: variabilidade, 2: sustentabilidade
+        'atraso': atraso,
+        'batch_size': 32,
+        'ent_coef': 0.0054570731886848,
+        'gae_lambda': 0.9576650603492444,
+        'gamma': 0.918455239357432,
+        'learning_rate': 0.04295168785001475,
+        'n_epochs': 7,
+        'n_steps': 2816,
+        'target_kl': 0.0062761358636258206
+    }
+
+    for x in range(1):    #### ACMO NUMEROS DE EXECUÇÕES COMPETIDORAS
+        run1 = wandb.init(project='Fred_test_SU', #NOME DO PROJETO
+                          config=config_PPO,
+                          group=f"priorizando {objetivo[config_PPO['objetivo']]}", #GRUPOS A SEREM ADCIONADOS NO WANDB
+                          name=f"PPO (teste 6, atraso = {atraso:02d}, run: {x + 5:02d})",
+                        #   name="run_test_SU", #NOME DA EXECUÇÃO
+                          save_code=True,
+                          reinit=True
+        )
+
+
+        config = wandb.config
+        env1 = DummyVecEnv([lambda: make_env(config.atraso, config.objetivo)])
+
+        model = PPO(
+            policy="MultiInputPolicy",
+            env=env1,
+            learning_rate=config.learning_rate,
+            n_steps=config.n_steps,
+            batch_size=config.batch_size,
+            n_epochs=config.n_epochs,
+            gamma=config.gamma,
+            gae_lambda=config.gae_lambda,
+            #clip_range=config.clip_range,
+            ent_coef=config.ent_coef,
+            #vf_coef=config.vf_coef,
+            #max_grad_norm=config.max_grad_norm,
+            target_kl=config.target_kl,
+            #stats_window_size=config.stats_window_size,
+            verbose=0,
+            #seed = 2023,
+            device='cpu',
+            #tensorboard_log=f"/content/drive/MyDrive/SOMN2/runs/{run1.id}"
+            tensorboard_log=f"runs/{run1.id}"
+        )
+        
+
+        model.learn(total_timesteps=333_334)
+        
+        # 1000 e verificar o tempo
+        # model.save(os.path.join(wandb.run.dir, f"model_custom_PPO (atraso = {atraso:02d}) run_{x+1:02d}"))
+        model.save(os.path.join(wandb.run.dir, f"model_custom_PPO (atraso = {atraso:02d}) run_{x+6:02d}"))
+        wandb.finish()
